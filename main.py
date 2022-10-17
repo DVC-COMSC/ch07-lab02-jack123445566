2fbc3be40757a555a7b6bab451980787f4a32e75
jack123445566 <112512759+jack123445566@users.noreply.github.com>
2022-10-17 01:43:55 +0000

    1st commit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-
-# ******************************
-# Make your Code
-# ******************************
+import random 
+numbers=[]
+for i in range(10): 
+    n = random.randint(0,10)
+    numbers.append(n)
+    smallest = min(numbers)
+print(f'Smallest element in the list is: {smallest}.')
